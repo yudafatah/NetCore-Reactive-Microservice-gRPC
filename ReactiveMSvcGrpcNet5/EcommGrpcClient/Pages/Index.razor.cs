d030fdf860e37dc2399cc7a454b9a942479e4ef9
fatah <yudafatah@gmail.com>
2021-03-15 18:16:45 +0700

    add grpc in client

---
+++ ReactiveMSvcGrpcNet5/EcommGrpcClient/Pages/Index.razor.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EcommGrpcClient.Pages
+{
+    public partial class Index
+    {
+        private IList<ProductGrpc.Protos.ProductModel> productModels;
+
+        protected override async Task OnInitializedAsync()
+        {
+            // for testing : waiting grpc server up
+            System.Threading.Thread.Sleep(5000);
+
+            var client = new ProductGrpc.Protos.ProductProtoService.ProductProtoServiceClient(Channel);
+
+            // request param
+            var getAllProducts = new ProductGrpc.Protos.GetAllProductReq();
+
+            // get all product streaming data from grpc
+            var resStream = client.GetAllProduct(getAllProducts);
+
+            // add each stream to productModels
+            List<ProductGrpc.Protos.ProductModel> temp = new List<ProductGrpc.Protos.ProductModel>();
+            await foreach (var item in resStream.ResponseStream.ReadAllAsync())
+            {
+                temp.Add(item);
+
+                Console.WriteLine(item);
+            }
+
+            productModels = temp;
+        }
+    }
+}
