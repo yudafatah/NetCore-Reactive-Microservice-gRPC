abc82fd0bb9671599ec54e140af2806a122a792c
fatah <yudafatah@gmail.com>
2021-03-15 18:26:27 +0700

    create product grpc

---
+++ ProductGrpc/Mapper/ProductProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
+using ProductGrpc.Models;
+using ProductGrpc.Protos;
+using static ProductGrpc.CQRS.Queries.ProductQuery;
+
+namespace ProductGrpc.Mapper
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductModel>()
+                .ForMember(x => x.CreatedTime, opt => opt.MapFrom(src => Timestamp.FromDateTime(src.CreatedTime)))
+                .ForMember(x => x.Status, opt => opt.MapFrom(src => (Protos.ProductStatus)src.Status));
+
+            CreateMap<ProductModel, Product>()
+                .ForMember(x => x.CreatedTime, opt => opt.MapFrom(src => src.CreatedTime.ToDateTime()))
+                .ForMember(x => x.Status, opt => opt.MapFrom(src => (Models.ProductStatus)src.Status));
+
+            CreateMap<GetProductQuery, GetProductReq>();
+            CreateMap<GetProductReq, GetProductQuery>();
+        }
+    }
+}
---
+++ ProductGrpc/Services/ProductService.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProductGrpc.Models;
+using ProductGrpc.Protos;
+using System;
+using System.Threading.Tasks;
+using static ProductGrpc.CQRS.Queries.ProductQuery;
+
+namespace ProductGrpc.Services
+{
+    public class ProductService : ProductProtoService.ProductProtoServiceBase
+    {
+        private readonly ProductContext productContext;
+        private readonly ILogger<ProductService> logger;
+        private readonly IMapper mapper;
+        private readonly IMediator mediator;
+
+        public ProductService(ProductContext productContext, ILogger<ProductService> logger, 
+            IMapper mapper, IMediator mediator)
+        {
+            this.productContext = productContext ?? throw new ArgumentNullException(nameof(productContext));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        public override Task<Empty> Test(Empty req, ServerCallContext context)
+        {
+            return base.Test(req, context);
+        }
+
+        public override async Task<ProductModel> GetProduct(GetProductReq request, ServerCallContext context)
+        {
+            //var product = await productContext.Product.FindAsync(request.ProductId);
+            var commandReq = mapper.Map<GetProductQuery>(request);
+
+            var product = await mediator.Send(commandReq);
+
+            if(product == null)
+            {
+                throw new RpcException(new Status(StatusCode.OK, "data empty"));
+            }
+
+            return await Task.FromResult(mapper.Map<ProductModel>(product));
+        }
+
+        public override async Task GetAllProduct(GetAllProductReq request, 
+            IServerStreamWriter<ProductModel> responseStream, ServerCallContext context)
+        {
+            var products = await productContext.Product.ToListAsync();
+
+            foreach (var product in products)
+            {
+                await responseStream.WriteAsync(mapper.Map<ProductModel>(product));
+            }
+        }
+
+        public override async Task<ProductModel> AddProduct(AddProductReq request, ServerCallContext context)
+        {
+            await productContext.Product.AddAsync(mapper.Map<Product>(request.Product));
+            await productContext.SaveChangesAsync();
+
+            return await Task.FromResult(request.Product);
+        }
+    }
+}
