9d90b2a9ce49534bab188e568994e6cdd8c58f05
fatah <yudafatah@gmail.com>
2021-03-17 22:32:22 +0700

    create sc worker service

---
+++ ShoppingCartGrpc/Services/DiscountService.cs
@@ -0,0 +1,22 @@
+﻿using DiscountGrpc.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace ShoppingCartGrpc.Services
+{
+    public class DiscountService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient discountProtoService;
+
+        public DiscountService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            this.discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+
+        public async Task<DiscountModel> GetDiscount(string discountCode)
+        {
+            var discountReq = new GetDiscountReq { DiscountCode = discountCode };
+            return await discountProtoService.GetDiscountAsync(discountReq);
+        }
+    }
+}
---
+++ ShoppingCartWorkerService/Worker.cs
@@ -0,0 +1,72 @@
+using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ShoppingCartGrpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShoppingCartWorkerService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration configuration;
+
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                using var scChannel = GrpcChannel.ForAddress(configuration["WorkerService:ShoppingCartGrpcUrl"]);
+                var scClient = new ShoppingCartProtoService.ShoppingCartProtoServiceClient(scChannel);
+
+                var scModel = await GetOrCreateSCAsync(scClient);
+
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(configuration.GetValue<int>("WorkerService:TaskInterval"), stoppingToken);
+            }
+        }
+
+        private async Task<ShoppingCartModel> GetOrCreateSCAsync(ShoppingCartProtoService.ShoppingCartProtoServiceClient scClient)
+        {
+            ShoppingCartModel shoppingCartModel;
+            try
+            {
+                _logger.LogInformation("worker Get Shopping Cart starting..");
+                shoppingCartModel = await scClient.GetShoppingCartAsync(new GetShoppingCartReq
+                {
+                    Username = configuration["WorkerService:Username"]
+                });
+                _logger.LogInformation("Get Shopping Cart Response : {response}", shoppingCartModel.ToString());
+            }
+            catch (RpcException exception)
+            {
+                if(exception.StatusCode == StatusCode.NotFound)
+                {
+                    _logger.LogInformation("worker Create Shopping Cart starting..");
+                    shoppingCartModel = await scClient.CreateShoppingCartAsync(new ShoppingCartModel
+                    {
+                        Username = configuration["WorkerService:Username"]
+                    });
+                    _logger.LogInformation("Create Shopping Cart Response : {response}",shoppingCartModel.ToString());
+                }
+                else
+                {
+                    throw exception;
+                }
+            }
+
+            return shoppingCartModel;
+        }
+    }
+}
